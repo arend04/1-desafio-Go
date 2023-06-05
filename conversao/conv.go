2e62bde3ec825ea90b0c27985555e4a124156486
Aline <aline.arend04@gmail.com>
2023-06-05 12:10:58 -0300

    Primeiro desafio do curso Go

---
+++ conversao/conv.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a int = 20
+var b string = "nome"
+
+func main() {
+
+	var c float64 = float64(a)
+	fmt.Printf("O valor de C é:  %g   e o valor de B é: %s  ", c, b)
+
+}
